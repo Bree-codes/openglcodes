60cbd9ddc806d57bb243832c4c194270b0aee79d
Brenda Mukami <breshy@localhost.localdomain>
2023-11-22 13:15:46 +0300

    opengl coloured hut program

---
+++ myhut.cpp
@@ -0,0 +1,88 @@
+#include <GL/glut.h>
+
+void display() {
+  glClear(GL_COLOR_BUFFER_BIT);
+	//draws the hut's wall borders
+  // Draw the hut's left wall
+  glBegin(GL_POINTS);
+  glColor3f(1.0f, 0.0f, 0.0f); // Red
+
+  for (float y = -50.0f; y <= 25.0f; y += 0.25f) {
+    glVertex2f(-75.0f, y);
+  }
+
+  glEnd();
+
+  // Draw the hut's bottom wall
+  glBegin(GL_POINTS);
+  glColor3f(0.0f, 1.0f, 0.0f); // Green
+
+  for (float x = -75.0f; x <= 75.0f; x += 0.25f) {
+    glVertex2f(x, -50.0f);
+  }
+
+  glEnd();
+
+  // Draw the hut's right wall
+  glBegin(GL_POINTS);
+  glColor3f(0.0f, 0.0f, 1.0f); // Blue
+
+  for (float y = -50.0f; y <= 25.0f; y += 0.25f) {
+    glVertex2f(75.0f, y);
+  }
+
+  glEnd();
+
+  // Draw the hut's roof
+  glBegin(GL_TRIANGLES);
+   glColor3f(0.0f, 0.0f, 1.0f);     //blue
+  glVertex3f(0.0f, 75.0f,0.0f);
+        glColor3f(1.0f, 0.0f, 0.0f);     //red
+
+  glVertex3f(-100.0f, 25.0f,0.0f);
+        glColor3f(0.0f, 1.0f, 0.0f);     // green
+
+  glVertex3f(100.0f, 25.0f,0.0f);
+  glEnd();
+
+	 // Draw the hut's walls to be colored
+  glBegin(GL_POLYGON);
+  //bottom left
+    glColor3f(1.0f, 0.0f, 0.0f);
+
+  glVertex2f(-74.0f, -49.0f);
+  //top left
+    glColor3f(0.0f, 0.0f, 1.0f);
+  glVertex2f(-74.0f, 25.0f);
+  //top right
+    glColor3f(0.0f, 0.0f, 1.0f);
+  glVertex2f(74.4f, 25.0f);
+  //bottom right
+    glColor3f(0.0f, 1.0f, 0.0f);
+
+  glVertex2f(74.4f, -49.0f);
+  glEnd();
+
+  glutSwapBuffers();
+}
+
+void reshape(int width, int height) {
+  glViewport(0, 0, width, height);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(-150.0f, 150.0f, -100.0f, 100.0f);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+}
+
+int main(int argc, char** argv) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+  glutInitWindowSize(500, 500);
+  glutInitWindowPosition(100, 100);
+  glutCreateWindow("MY HUT");
+  glutDisplayFunc(display);
+  glutReshapeFunc(reshape);
+  glutMainLoop();
+  return 0;
+}
